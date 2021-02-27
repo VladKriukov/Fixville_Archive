5b4225150dedac17b91a7f3c039a220eb8f1ec1f
VladKriukov <vladik.kriukov@gmail.com>
2021-02-27 19:33:30 +0000

    Added project to GitHub

---
+++ Fixville/Assets/Scripts/NavMeshAgentSetDestination.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentSetDestination : MonoBehaviour {
+
+	NavMeshAgent navmeshAgent;
+
+	void Start () {
+		navmeshAgent = GetComponent<NavMeshAgent> ();
+	}
+
+	public void SetAgentDestinationTo (Vector3 pos) {
+        navmeshAgent.destination = pos;
+	}
+
+}
